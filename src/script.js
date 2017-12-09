ad4438def4e71b6f75b8f1755861293ce84058e2
Farid El Nasire <iam@faridelnasire.com>
2017-12-09 18:39:21 -0500

    Testing database connection

--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
 $(function() {
+  var Database = {
+    database_url: 'database.csv',
+    search: function(input_string) {
+      $.ajax({
+        url: self.database_url,
+        async: false,
+        success: function(result) {
+          return $.parseJSON(result);
+        }
+      });
+    }
+  };
+
   var search_input = $('header#page-header input[type="text"].search-bar');
 
   search_input.on('input', function() {
-    if($(this).val().length) {
+    var search_input = $(this).val();
+
+    if(search_input.length) {
       $('body').addClass('searching');
+
+      var results = Database.search(search_input);
+      console.log(results);
     } else {
       $('body').removeClass('searching');
     }
